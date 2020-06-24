bca7f579e272536579356d98a3e80aa946ea706f
jorge C <jcraviotto@hotmail.com>
2020-06-24 16:47:38 -0300

    ADD: initial commit

---
+++ src/index.php
@@ -0,0 +1,14 @@
+<?php 
+
+namespace astroselling\Jupiter;
+
+class Index
+{
+	private $version = "Jupiter SDK v1.00 beta";
+	
+    public function version()
+    {
+        return $this->version;
+    }
+}
+
